c4c900a64259f04423b16d4b797d0404267b6537
Quy Nguyen <nhonquy@gmail.com>
2021-04-04 09:44:43 -0700

    add some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/share/header/header.component';
+import { FooterComponent } from './components/share/footer/footer.component';
+import { PagenotfoundComponent } from './components/share/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
